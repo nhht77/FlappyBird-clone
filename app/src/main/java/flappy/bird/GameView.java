b9f99e7628170bf24a5f31b87e35f809af6938e5
nhht77 <35263094+nhht77@users.noreply.github.com>
2019-02-21 17:56:22 +0200

    Screen fit

--- app/src/main/java/flappy/bird/GameView.java
+++ app/src/main/java/flappy/bird/GameView.java
@@ -1,9 +1,13 @@
 package flappy.bird;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.view.Display;
 import android.view.View;
 import android.os.Handler;
 
@@ -13,6 +17,10 @@ public class GameView extends View {
     Runnable runnable;
     final int UPDATE_MILIS=30;
     Bitmap background;
+    Display display;
+    Point point;
+    int dWidth, dHeight;
+    Rect rect;
 
     public GameView(Context context){
         super(context);
@@ -24,13 +32,20 @@ public class GameView extends View {
             }
         };
         background = BitmapFactory.decodeResource(getResources(), R.drawable.bg);
+        display    = ((Activity)getContext()).getWindowManager().getDefaultDisplay();
+        point      = new Point();
+        display.getSize(point);
+        dWidth = point.x;
+        dHeight = point.y;
+        rect = new Rect(0, 0, dWidth, dHeight);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawBitmap(background, 0, 0, null);
+//        canvas.drawBitmap(background, 0, 0, null);
+        canvas.drawBitmap(background, null, rect, null);
         handler.postDelayed(runnable, UPDATE_MILIS);
     }
 }
